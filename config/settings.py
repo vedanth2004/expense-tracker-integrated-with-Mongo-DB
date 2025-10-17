56110d3e1aa5916883648951028af5f9e08e883e
Vedanth Reddy <121875135+vedanth2004@users.noreply.github.com>
2025-10-17 21:53:25 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,14 @@
 from pydantic_settings import BaseSettings
-from typing import Optional
+import os
 import streamlit as st
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = st.secrets.get("SECRET_KEY", "dev_secret_key")
-    MONGO_URI: str = st.secrets.get("MONGO_URI", "mongodb+srv://shapuramvedanthreddy_db_user:kyNIkrEaOzK5Ovuc@cluster0.7e1g9hm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    SMTP_HOST: str = st.secrets.get("SMTP_HOST", "smtp.gmail.com")
-    SMTP_PORT: int = st.secrets.get("SMTP_PORT", 587)
-    SMTP_USER: str = st.secrets.get("SMTP_USER", "")
-    SMTP_PASS: str = st.secrets.get("SMTP_PASS", "")
-    CURRENCY_BASE: str = st.secrets.get("CURRENCY_BASE", "USD")
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    SECRET_KEY: str = os.getenv("SECRET_KEY", None) or st.secrets.get("SECRET_KEY", "dev_secret_key")
+    MONGO_URI: str = os.getenv("MONGO_URI", None) or st.secrets.get("MONGO_URI", None)
+    SMTP_HOST: str = os.getenv("SMTP_HOST", "smtp.gmail.com")
+    SMTP_PORT: int = int(os.getenv("SMTP_PORT", 587))
+    SMTP_USER: str = os.getenv("SMTP_USER", None) or st.secrets.get("SMTP_USER", "")
+    SMTP_PASS: str = os.getenv("SMTP_PASS", None) or st.secrets.get("SMTP_PASS", "")
+    CURRENCY_BASE: str = os.getenv("CURRENCY_BASE", None) or st.secrets.get("CURRENCY_BASE", "USD")
 
 settings = Settings()
